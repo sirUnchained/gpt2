77e45c7fbefffb3998d11bca9c76f1d0bff6eef0
Behnam Bahadorpour <bbehnam3831@gmail.com>
2026-08-28 11:21:28 +0330

    fixed the scilent bug!

--- src/models/transformer_block.py
+++ src/models/transformer_block.py
@@ -50,7 +50,7 @@ class TransformerBlock(nn.Module):
             qkv_bias=cfg.qkv_bias,
         )
 
-        self.dropout = nn.Dropout(p=0)
+        self.dropout = nn.Dropout(p=dropout)
 
         self.norm_1 = LayerNormalizer(emb_dim=cfg.emb_dim)
 
